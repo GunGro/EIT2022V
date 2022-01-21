6dc5da1ab02f35c184d1188670dd173c7e1a9166
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2022-01-21 12:10:26 +0100

    save

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,19 +111,19 @@ if not do_deterministic:
         )
 else:
     st.markdown('# <span style="color:#410464">Our use of AI </span>', unsafe_allow_html=True)
+    st.write("""
+    Student loan fraud costs Lånekassen several million a year. In particular, applicants may falsely state that they are living away from home, and thus be eligible for an education grant (stipend). Lånekassen estimates that a recurring 4-5 % of students cannot prove that they’re living away from home. \n
+To combat this problem, our team has developed an AI that will suggest high-risk cases of student loan holders, whose applications will be processed manually. This initiative will help us allocate our resources more efficiently. Both in terms of time needed to process the student loan applications, and in terms of funds distributed. In the long run, this will lead to more efficient use of tax money and give more people the opportunity to get an education. \n
+To gain trust with our applicants, we strive to provide insight into how our AI suggests high-risk cases. Therefore, we have calculated the relative importance of the variables used by our AI in determining the risk estimate. \n
+In our risk calculator widget to the right, you see the most important variables in our AI model. To better understand how our model calculates risk, we invite you to manipulate the variables to the right and see how the risk estimate changes. 
+"""
+    )
     col1, col2= st.columns([3, 2])
     with col2:
         st.markdown('## <span style="color:#410464"> Risk Calculator</span>', unsafe_allow_html=True)
 
     col1, placeholder, col2,  col3 = st.columns([3, 0.2, 1, 1])
     with col1:
-        st.write("""
-        Student loan fraud costs Lånekassen several million a year. In particular, applicants may falsely state that they are living away from home, and thus be eligible for an education grant (stipend). Lånekassen estimates that a recurring 4-5 % of students cannot prove that they’re living away from home. \n
-    To combat this problem, our team has developed an AI that will suggest high-risk cases of student loan holders, whose applications will be processed manually. This initiative will help us allocate our resources more efficiently. Both in terms of time needed to process the student loan applications, and in terms of funds distributed. In the long run, this will lead to more efficient use of tax money and give more people the opportunity to get an education. \n
-    To gain trust with our applicants, we strive to provide insight into how our AI suggests high-risk cases. Therefore, we have calculated the relative importance of the variables used by our AI in determining the risk estimate. \n
-    In our risk calculator widget to the right, you see the most important variables in our AI model. To better understand how our model calculates risk, we invite you to manipulate the variables to the right and see how the risk estimate changes. 
-    """
-        )
         st.markdown("## <span style='color:#410464'>Feature Importance </span>", unsafe_allow_html=True)
         st.write("Feature Importance refers to techniques that calculate a score for all the input features (variables) for a given model — the scores simply represent the “importance” of each feature. A higher score means that the specific feature will have a larger effect on the model that is being used to predict the risk.")
         st.pyplot(fig = feat_fig, facecolor="#F4F4F4")
