9a0321117c0359f447843ca427f0f7b74742dc4d
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2022-01-24 10:44:16 +0100

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ st.set_page_config(
         layout="wide"
 )
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css('./style.css')
 st.image('./header_english.png')
 
 col1, col2, col3, col4 = st.columns(4)
