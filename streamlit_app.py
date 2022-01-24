156eaa45cbe3ad712a83d41f46a9fb3fd341a533
henrik <henrik.furuheim@gmail.com>
2022-01-24 10:49:50 +0100

    Knapp

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,10 @@ col1, col2, col3 = st.columns([0.5, 1, 0.5])
 with col2:
     st.markdown('# <span style="color:#410464"> Lånekassens AI</span>', unsafe_allow_html=True)
     st.write("""
-    Student loan fraud costs Lånekassen several million a year. In particular, applicants may falsely state that they are living away from home, and thus be eligible for an education grant (stipend). Lånekassen estimates that a recurring 4-5 % of students cannot prove that they’re living away from home. \n
+    Student loan fraud costs Lånekassen several million kroner a year. In particular, applicants may falsely state that they are living away from home, and thus receive an education grant (stipend) they’re not eligible for. Lånekassen estimates that a recurring 4-5 % of students cannot prove that they’re living away from home.\n 
 To combat this problem, our team has developed an AI that will suggest high-risk cases of student loan holders, whose applications will be processed manually. This initiative will help us allocate our resources more efficiently. Both in terms of time needed to process the student loan applications, and in terms of funds distributed. In the long run, this will lead to more efficient use of tax money and give more people the opportunity to get an education. \n
-To gain trust with our applicants, we strive to provide insight into how our AI suggests high-risk cases. Therefore, we have calculated the relative importance of the variables used by our AI in determining the risk estimate. \n
-In our risk calculator widget you see the most important variables in our AI model. To better understand how our model calculates risk, we invite you to manipulate the variables and see how the risk estimate changes.
+To promote responsible and transparent use of AI we allow users to calculate their own risk here before they fill out their application. 
+
 """
     )
 
