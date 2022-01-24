421963a63138ba64b06af4ce947d1d2209d8c2db
henrik <henrik.furuheim@gmail.com>
2022-01-24 10:16:21 +0100

    Knapp

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ col1, col2, col3 = st.columns(3)
 
 with col2:
         st.title("Lånekassens AI")
-        st.write("""
+        st.subheader("""
         Student loan fraud costs Lånekassen several million a year. In particular, applicants may falsely state that they are living away from home, and thus be eligible for an education grant (stipend). Lånekassen estimates that a recurring 4-5 % of students cannot prove that they’re living away from home. \n
     To combat this problem, our team has developed an AI that will suggest high-risk cases of student loan holders, whose applications will be processed manually. This initiative will help us allocate our resources more efficiently. Both in terms of time needed to process the student loan applications, and in terms of funds distributed. In the long run, this will lead to more efficient use of tax money and give more people the opportunity to get an education. \n
     To gain trust with our applicants, we strive to provide insight into how our AI suggests high-risk cases. Therefore, we have calculated the relative importance of the variables used by our AI in determining the risk estimate. \n
